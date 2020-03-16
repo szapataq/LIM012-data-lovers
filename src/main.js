f13f2c753aa14108d17972ea6c983b76730d0919
szapataq <szapata013@gmail.com>
2020-03-16 18:36:07 -0500

    corrigiendo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
+/**import { example } from './data.js';*/
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+
+
+let pokemonList = data.pokemon;
+let containerPokemon = document.querySelector("#container-pokemon");
+
+Object.values(pokemonList).map( (pokem) =>{
+    const divGroup = document.createElement("div");
+    const divImg = document.createElement("div");
+    const divInfo = document.createElement("div");
+    const img = document.createElement ("img");
+    const p = document.createElement("p");
+
+    divGroup.className = "pokemon-group";
+    divImg.className = "pokemon-img";
+    divInfo.className = "pokemon-info";
+    p.className = "pokemon-name";
+
+    img.src = `${pokem.img}`;
+    p.innerHTML = `${pokem.name}`;
+
+    divImg.appendChild(img);
+    divInfo.appendChild(p);
+    divGroup.appendChild(divImg);
+    divGroup.appendChild(divInfo);
+    containerPokemon.appendChild(divGroup);
+
+});
+
+
+
+
+
+
