784a77bd1ebf5b2722c81d1b58c3d2b4c195ae3c
szapataq <szapata013@gmail.com>
2020-03-16 09:50:38 -0500

    actualizacion de archivos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
-import { pokemon } from './data/pokemon/pokemon.js';
+/**import { example } from './data.js';*/
+import data from './data/pokemon/pokemon.js';
 
-let number = pokemon.filter(pokemon => pokemon.name);
-console.log(number);
+let pokemonList = data.pokemon;
+let containerPokemon = document.querySelector("#container-pokemon");
+
+
+Object.values(pokemonList).map( (pokem) =>{
+
+    const divGroup = document.createElement("div");
+    const divImg = document.createElement("div");
+    const divInfo = document.createElement("div");
+    const img = document.createElement ("img");
+    const p = document.createElement("p");
+
+    divGroup.className = "pokemon-group";
+    divImg.className = "pokemon-img"
+    divInfo.className = "pokemon-info"
+    p.className = "pokemon-name";
+
+    img.src = `${pokem.img}`;
+    p.innerHTML = `${pokem.name}`;
+
+    divImg.appendChild(img);
+    divInfo.appendChild(p);
+
+    divGroup.appendChild(divImg);
+    divGroup.appendChild(divInfo);
+
+    containerPokemon.appendChild(divGroup);
+
+});
+
+
+
+
+
+
