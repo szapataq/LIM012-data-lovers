c70aa3965cd703562c83e073457d08ddfc6e0f28
szapataq <szapata013@gmail.com>
2020-03-18 13:32:29 -0500

    mostrando los card en plantilla de texto

--- src/data.js
+++ src/data.js
@@ -2,4 +2,8 @@
 
 export const example = () => 'example';
 
+export const searchPokemon = (listPokemon, input) => {
+  const pokemon = listPokemon.filter(poke => poke.toLowerCase().startsWith(input));
+  return pokemon;
+};
 export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,36 +1,21 @@
-/**import { example } from './data.js';*/
 import data from './data/pokemon/pokemon.js';
 
+const pokemonList = data.pokemon;
 
-
-let pokemonList = data.pokemon;
-let containerPokemon = document.querySelector("#container-pokemon");
-
-Object.values(pokemonList).map( (pokem) =>{
-    const divGroup = document.createElement("div");
-    const divImg = document.createElement("div");
-    const divInfo = document.createElement("div");
-    const img = document.createElement ("img");
-    const p = document.createElement("p");
-
-    divGroup.className = "pokemon-group";
-    divImg.className = "pokemon-img";
-    divInfo.className = "pokemon-info";
-    p.className = "pokemon-name";
-
-    img.src = `${pokem.img}`;
-    p.innerHTML = `${pokem.name}`;
-
-    divImg.appendChild(img);
-    divInfo.appendChild(p);
-    divGroup.appendChild(divImg);
-    divGroup.appendChild(divInfo);
-    containerPokemon.appendChild(divGroup);
-
-});
-
-
-
-
-
-
+const showPokemon = (list) => {
+  const containerPokemon = document.getElementById('container-pokemon');
+  Object.values(list).map((pokem) => {
+    const card = `          
+      <div class="pokemon-group">
+        <div class="pokemon-img">
+          <img src="${pokem.img}">
+        </div>
+        <div class="pokemon-info">
+          <p>${pokem.name}</p>
+          <p>${pokem.num}</p>
+        </div>
+      </div> `;
+    containerPokemon.innerHTML += card;
+  });
+};
+showPokemon(pokemonList);
