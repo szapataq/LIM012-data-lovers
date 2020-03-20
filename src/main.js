1f43caab5b3771c037a1db0a879698d3dc59a3a2
szapataq <szapata013@gmail.com>
2020-03-20 10:11:44 -0500

    responsive de cards

--- src/main.js
+++ src/main.js
@@ -3,21 +3,22 @@ import data from './data/pokemon/pokemon.js';
 const pokemonList = data.pokemon;
 
 const showPokemon = (list) => {
-  const containerPokemon = document.getElementById('container-pokemon');
+  const containerPokemon = document.getElementById('container-card');
   Object.values(list).map((pokem) => {
-    const card = `          
-      <div class="pokemon-group">
-        <div class="pokemon-img">
-          <img src="${pokem.img}">
-        </div>
-        <div class="pokemon-info">
-          <p class="poke-name">${pokem.name}</p>
-          <p class="poke-num"> N° ${pokem.num}</p>
-          <p class="poke-info"> CP Máx: ${pokem.stats['max-cp']}</p>
-          <p class="poke-info"> HP Máx: ${pokem.stats['max-hp']}</p>
-        </div>
-      </div> `;
+    const card = `
+    <div class="pokemon-group">
+    <div class="poke-img">
+      <img src="${pokem.img}">
+    </div>
+    <div class="container-info">
+      <p class="poke-name bold">${pokem.name}</p>
+      <p class="poke-num"> N° ${pokem.num}</p>
+      <p class="poke-info bold"> CP Máx: ${pokem.stats['max-cp']}</p>
+      <p class="poke-info bold"> HP Máx: ${pokem.stats['max-hp']}</p>
+    </div>
+  </div>`;
     containerPokemon.innerHTML += card;
   });
+  return containerPokemon;
 };
 showPokemon(pokemonList);
