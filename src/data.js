eb285df679be57b121204ff4ae0da6d96a1707a9
szapataq <szapata013@gmail.com>
2020-03-22 19:59:08 -0500

    filter function by type of pokemon

--- src/data.js
+++ src/data.js
@@ -7,3 +7,17 @@ export const searchPokemon = (listPokemon, input) => {
   return pokemon;
 };
 export const anotherExample = () => 'OMG';
+
+
+export const filterByType = (ArrayObj, elementType) => {
+  const arrayTipeFilter = [];
+  for (let i = 0; i < ArrayObj.length; i += 1) {
+    const obj = ArrayObj[i].type;
+    for (let j = 0; j < obj.length; j += 1) {
+      if (obj[j] === elementType) {
+        arrayTipeFilter.push(ArrayObj[i]);
+      }
+    }
+  }
+  return arrayTipeFilter;
+};
--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
 import data from './data/pokemon/pokemon.js';
+import {
+  filterByType,
+} from './data.js';
 
 const pokemonList = data.pokemon;
+const containerPokemons = document.getElementById('container-card');
 
 const showPokemon = (list) => {
   const containerPokemon = document.getElementById('container-card');
-  Object.values(list).map((pokem) => {
+  list.forEach((pokem) => {
     const card = `
     <div class="pokemon-group">
-    <div class="poke-img">
-      <img src="${pokem.img}">
-    </div>
-    <div class="container-info">
-      <p class="poke-name bold">${pokem.name}</p>
-      <p class="poke-num"> N° ${pokem.num}</p>
-      <p class="poke-info bold"> CP Máx: ${pokem.stats['max-cp']}</p>
-      <p class="poke-info bold"> HP Máx: ${pokem.stats['max-hp']}</p>
-    </div>
-  </div>`;
+      <div class="poke-img">
+        <img src="${pokem.img}">
+      </div>
+      <div class="container-info">
+        <p class="poke-name bold">${pokem.name}</p>
+        <p class="poke-num"> N° ${pokem.num}</p>
+        <p class="poke-info bold"> CP Máx: ${pokem.stats['max-cp']}</p>
+        <p class="poke-info bold"> HP Máx: ${pokem.stats['max-hp']}</p>
+      </div>
+    </div>`;
     containerPokemon.innerHTML += card;
   });
   return containerPokemon;
 };
+
 showPokemon(pokemonList);
+const elementTypeFilter = document.getElementById('element-type-filter');
+elementTypeFilter.addEventListener('change', () => {
+  const catchFilter = filterByType(pokemonList, elementTypeFilter.value);
+  containerPokemons.innerHTML = '';
+  showPokemon(catchFilter);
+});
