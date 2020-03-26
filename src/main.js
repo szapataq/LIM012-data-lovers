b3215670eaa1963c7b47d8567639b87187b241d6
szapataq <szapata013@gmail.com>
2020-03-26 08:59:19 -0500

    filert water corregido

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ const showPokemon = (list) => {
         <img src="${pokem.img}">
       </div>
       <div class="container-info">
-        <p class="poke-name ">${pokem.name}</p>
+        <p class="poke-name">${pokem.name}</p>
         <p class="poke-num"> N° ${pokem.num}</p>
         <p class="poke-info bold"> CP Máx: ${pokem.stats['max-cp']}</p>
         <p class="poke-info bold"> HP Máx: ${pokem.stats['max-hp']}</p>
