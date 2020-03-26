a7375c044d577a1fc63f2e30095383eb105a6a65
szapataq <szapata013@gmail.com>
2020-03-26 00:57:11 -0500

    ultimas modificaciones

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ const inputSearch = document.getElementById('search');
 
 const showPokemon = (list) => {
   const containerPokemon = document.getElementById('container-card');
+  let count = 0;
   list.forEach((pokem) => {
     const card = `
     <div class="pokemon-group">
@@ -19,41 +20,50 @@ const showPokemon = (list) => {
         <img src="${pokem.img}">
       </div>
       <div class="container-info">
-        <p class="poke-name bold">${pokem.name}</p>
+        <p class="poke-name ">${pokem.name}</p>
         <p class="poke-num"> N° ${pokem.num}</p>
         <p class="poke-info bold"> CP Máx: ${pokem.stats['max-cp']}</p>
         <p class="poke-info bold"> HP Máx: ${pokem.stats['max-hp']}</p>
       </div>
     </div>`;
     containerPokemon.innerHTML += card;
+    count += 1;
   });
+  document.getElementById('quantity').innerHTML = count;
   return containerPokemon;
 };
-
+// muestra todos los pokemon
 showPokemon(pokemonList);
 
+// muestra los tipos de pokemon seleccionando el select type
 elementTypeFilter.addEventListener('change', () => {
   if (elementTypeFilter.value === 'all') {
     containerPokemons.innerHTML = '';
     showPokemon(pokemonList);
+  } else {
+    const catchFilter = filterByType(pokemonList, elementTypeFilter.value);
+    containerPokemons.innerHTML = '';
+    showPokemon(catchFilter);
   }
-  const catchFilter = filterByType(pokemonList, elementTypeFilter.value);
-  containerPokemons.innerHTML = '';
-  showPokemon(catchFilter);
 });
-
+// muestra los pokemon buscados atravez del input
 btnSearch.addEventListener('click', () => {
   containerPokemons.innerHTML = '';
   showPokemon(searchPokemonByName(pokemonList, inputSearch.value));
 });
 
-/* pokemonList.forEach((obj) => {
-  const requiredData = [];
-  console.log(obj.img);
-  console.log(obj.name);
-  console.log(obj.num);
-  console.log(obj.img);
-  console.log(obj.stats['max-cp']);
-  console.log(obj.stats['max-hp']);
+// muestras las acciones del botton para que suba la pantalla
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 100) {
+    document.querySelector('.container-btn-top').classList.add('show');
+  } else {
+    document.querySelector('.container-btn-top').classList.remove('show');
+  }
+};
+
+document.querySelector('.container-btn-top').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 });
-*/
